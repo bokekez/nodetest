20caee0ea9ea291ff862a8213a50753e7767e358
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-08 19:01:38 +0100

    1

--- test.js
+++ test.js
@@ -4,5 +4,26 @@ fs.readFile('./hello.text', (err, data) =>{
     if (err) {
         console.log('error');
     }
-    console.log(data.toString());
+    console.log('1', data.toString());
+})
+
+const file = fs.readFileSync('./hello.text');
+console.log('2', file.toString()); 
+
+// fs.appendFile('./hello.text', ' This is cool!!1', err => {
+//     if (err) {
+//         console.log(err)
+//     }
+// })
+
+fs.writeFile('bye.txt', 'See ya later', err => {
+    if (err) {
+        console.log(err)
+    }
+})
+
+fs.unlink('./bye.txt', err => {
+   if (err){
+        console.log(err)
+   } 
 })
