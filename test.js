d60e9cdd17421079825915498de1f48b35306eb4
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-09 12:18:24 +0100

    1

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('working!');
+})
+
+app.listen(3000, ()=>{
+    console.log('working');
+})
+
 const fs = require('fs');
 
 fs.readFile('./hello.text', (err, data) =>{
