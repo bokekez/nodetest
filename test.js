a7fe09a30308cc38fd847a7ac966280f4dbf7200
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-03-09 14:45:28 +0100

    1

--- test.js
+++ test.js
@@ -1,10 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+const data = {
+    users: [
+        {
+            id: '1',
+            name: 'Johny',
+            email: 'Johny@gmail.com',
+            password: 'Johne',
+            joined: new Date()
+        },
+        {
+            id: '2',
+            name: 'Annie',
+            email: 'Annie@gmail.com',
+            password: 'Ana',
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) =>{
     res.send('working!');
 })
+
+app.post('/signin', (req, res) =>{
+    if(req.body.email === data.users[0].email && 
+        req.body.password === data.users[0].password){
+            res.json('Good!');
+        } else {
+            res.json(400).json('Bad!');
+        }
+})
+
+// app.post('/signin', (req, res) =>{
+//     res.send('signing')
+// })
 
 app.listen(3000, ()=>{
     console.log('working');
